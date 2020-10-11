4a2554e76827dc8bac2f069bf2fa5a1a3a951750
crt10 <creatortenten2@gmail.com>
2020-10-11 01:17:58 -0400

    extra credit
    -provided social links
    -updated title field
    -changed background color

--- index.js
+++ index.js
@@ -5,13 +5,19 @@ addEventListener('fetch', event => {
 async function handleRequest(request) {
   const host = "https://cloudflare-2020-general-engineering-assignment.tennyson-cheng.workers.dev"
   const url = host + "/links"
+  const linkTreeURL = "https://static-links-page.signalnerve.workers.dev"
   const links = [
     JSON.stringify({name: "Github", url: "https://github.com/crt10"}),
     JSON.stringify({name: "LinkedIn", url: "https://www.linkedin.com/in/tennyson-cheng-bb053b1b8/"}),
     JSON.stringify({name: "Personal Site", url: "https://meme-werld.fun"})
   ]
+  const socialLinks = [
+    JSON.stringify({name: "Facebook", url: "https://www.facebook.com/tennyson.cheng", svg: "https://simpleicons.org/icons/facebook.svg"}),
+    JSON.stringify({name: "Twitch", url: "https://www.twitch.tv/creatorten", svg: "https://simpleicons.org/icons/twitch.svg"}),
+  ]
   const pfp = "https://meme-werld.fun/profile.jpg"
   const name = "Tennyson Cheng"
+  const color = "bg-blue-700"
 
   if (request.url === url) {
     return new Response(links, {
@@ -23,11 +29,14 @@ async function handleRequest(request) {
   .on("div#profile", new RewriteAttribute("style"))
   .on("img#avatar", new RewriteAttribute("src", pfp))
   .on("h1#name", new RewriteAttribute("", name))
-  .transform(await fetchStaticHTML())
+  .on("div#social", new RewriteAttribute("style"))
+  .on("div#social", new LinksTransformer(socialLinks))
+  .on("title", new RewriteAttribute("", name))
+  .on("body", new RewriteAttribute("class", color))
+  .transform(await fetchStaticHTML(linkTreeURL))
 }
 
-async function fetchStaticHTML() {
-  const url = "https://static-links-page.signalnerve.workers.dev"
+async function fetchStaticHTML(url) {
   const response = await fetch(url, {
     headers: {"content-type": "text/html"},
   })
@@ -41,9 +50,15 @@ class LinksTransformer {
     this.links = links
   }
   async element(e) {
-    console.log(`Incoming element: ${e.tagName}`)
     for (let link of this.links) {
-      let a = "<a href=\"" + JSON.parse(link).url + "\">" + JSON.parse(link).name + "</a>"
+      let a = "<a href=\"" + JSON.parse(link).url + "\">"
+      if (JSON.parse(link).svg) {
+        a += await (await fetchStaticHTML(JSON.parse(link).svg)).text()
+      }
+      else {
+        a += JSON.parse(link).name
+      }
+      a += "</a>"
       e.append(a, {html:true})
     }
   }
@@ -63,7 +78,10 @@ class RewriteAttribute {
         e.setAttribute(this.attr, this.param)
         break
       case "":
-        e.append(this.param, {html: false})
+        e.setInnerContent(this.param, {html: false})
+        break
+      case "class":
+        e.setAttribute(this.attr, this.param)
     }
   }
 }
